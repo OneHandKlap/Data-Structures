07f556d3413297a1fa4df7567d45abd2ff7a5cbc
OneHandKlap <45212899+OneHandKlap@users.noreply.github.com>
2019-09-09 16:27:23 -0400

    eh

--- lab01/mySolution/__init__.py
+++ lab01/mySolution/__init__.py
@@ -1,20 +1,44 @@
 import random
 
 def generate(str_len):
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    alphabet = ' abcdefghijklmnopqrstuvwxyz'
     res =''
     for i in range(str_len):
         res = res + alphabet[random.randrange(27)]
     return res
 
-print(generate(15))
+#print(generate(15))
 
 def calculate_score(goal,test):
     num_of_correct_characters = 0
+    correct_places=[]
     for i in range(len(test)):
         if goal[i] == test[i]:
             num_of_correct_characters+=1
+            correct_places+=i
     result = num_of_correct_characters/len(test)
     return result
 
-print(calculate_score('me thinks it looks like a weasel','i am an idiot and dont know it'))
+#print(calculate_score('me thinks it looks like a weasel','i am an idiot and dont know it'))
+
+def monkeyTypist():
+    target = list('methinks it is like a weasel')
+    alphabet = list(' abcdefghijklmnopqrstuvwxyz')
+    best_string = generate(28)
+    best_score = calculate_score(target,best_string)
+    attempt_counter = 0
+    print('String\t\t\t\t\t\tScore')
+    while best_score<100:
+        if best_score==100:
+            break
+        next_str = generate(28)
+        next_score = calculate_score(target,next_str)
+        if next_score>best_score:
+            best_score=next_score
+            best_string=next_str
+        if attempt_counter==100:
+            print("%s   %d" %   ("".join(best_string),best_score))
+            break
+        attempt_counter+=1
+
+monkeyTypist()
