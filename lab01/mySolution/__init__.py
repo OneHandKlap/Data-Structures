2ed8cad3df876c9e6a9f0bf4e0f751e91930f8a3
OneHandKlap <45212899+OneHandKlap@users.noreply.github.com>
2019-09-10 17:15:35 -0400

    10-Sep-2019

--- lab01/mySolution/__init__.py
+++ lab01/mySolution/__init__.py
@@ -2,10 +2,7 @@ import random
 from collections import OrderedDict
 import matplotlib.pyplot as plt
 
-
-
-
-def generate(str_len,dictionary_of_matches=0,change_one=0,string=0):
+def generate(str_len,dictionary_of_matches=0):
     alphabet = ' abcdefghijklmnopqrstuvwxyz'
     res =''
     if dictionary_of_matches:
@@ -32,9 +29,6 @@ def change_one(string,target):
             changed_one=True
     return res
 
-
-#print(generate(15))
-
 def calculate_score(goal,test):
     num_of_correct_characters = 0
     correct_places= OrderedDict()
@@ -44,14 +38,14 @@ def calculate_score(goal,test):
             correct_places[i]=test[i]
     return correct_places
 
-#print(calculate_score('me thinks it looks like a weasel','i am an idiot and dont know it'))
-
 def monkeyTypist():
     target = list('methinks it is like a weasel')
     alphabet = list(' abcdefghijklmnopqrstuvwxyz')
     best_string = generate(28)
     best_score = float(len(calculate_score(target,best_string))/len(best_string))*100
     attempt_counter = 0
+    indexes=[0]
+    scores=[best_score]
     print('String\t\t\t\t\t\tScore')
     while best_score<100:
         if best_score==100:
@@ -61,13 +55,18 @@ def monkeyTypist():
         if next_score>best_score:
             best_score=next_score
             best_string=next_str
+            indexes.append(attempt_counter)
+            scores.append(best_score)
         if attempt_counter==100000:
             break
         if attempt_counter%100==0:
             print("%s  \t\t\t %d" %   ("".join(best_string),best_score))
         attempt_counter+=1
+    ax1=plt.plot(indexes,scores,'r+')
+    plt.xlabel('Iteration')
+    plt.ylabel('Score of Best String')
+    plt.ylim(0,100)
+    plt.xscale
+    plt.show()
 
-#print(calculate_score('test','tee'))
 monkeyTypist()
-
-#print(change_one('test','teeee'))
