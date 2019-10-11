f6e9c26cb4d5fb52095dbe0a538b7b55e330c3f8
A. Aboud <a.aboud10@gmail.com>
2019-10-11 10:32:29 -0400

    11-Oct-2019

--- Chapter6.py
+++ Chapter6.py
@@ -1,6 +1,5 @@
 def bubble_sort(arr):
     for i in range(len(arr)-1):
-        print(i)
         swapped_items=False
         for j in range(len(arr)-1):
             if arr[j]>arr[j+1]:
@@ -10,8 +9,6 @@ def bubble_sort(arr):
             return arr
     return arr
 
-# l=[3,9,3,4,6,5]
-# print(bubble_sort(l))
 
 def recursive_bubble_sort(arr,flag=True,count=0):
     if flag==False:
@@ -25,24 +22,59 @@ def recursive_bubble_sort(arr,flag=True,count=0):
         count+=1
         return recursive_bubble_sort(arr,flag,count)
 
-# l=[3,9,3,4,6,5]
-# print(recursive_bubble_sort(l))
 
 def selection_sort(arr):
     for j in range(len(arr)):
-        print("pass number:"+str(j))
         swap_items=False
         largest_index=0
         for i in range(len(arr)-j):
-            print("item number:"+str(i))
             if arr[i]>arr[largest_index]:
                 largest_index=i
                 swap_items=True
         if swap_items:
-            #print(arr[len(arr)-1-j])
             arr[len(arr)-1-j],arr[largest_index]=arr[largest_index],arr[len(arr)-1-j]
     return arr
 
+def recursive_sel_sort(arr,flag=True,count=0):
+    if flag==False or count==len(arr):
+        return arr
+    else:
+        flag==False
+        largest=max(arr[:len(arr)-count])
+        index_largest=arr.index(largest)
+        arr[len(arr)-1-count],arr[index_largest]=arr[index_largest],arr[len(arr)-1-count]
+        flag=True
+        count+=1
+        return (recursive_sel_sort(arr,flag,count))
+
 l=[3,9,3,4,6,5]
-print(selection_sort(l))
+#print(recursive_sel_sort(l))
+
+
+def josephus(num_prisoners,num_candy,start_pos):
+    end_pos=(num_candy%num_prisoners)+start_pos-1
+    print(end_pos)
+    if end_pos<num_prisoners:
         
+        return end_pos
+    else:
+        return end_pos%num_prisoners
+
+
+#print(josephus(88325121,435452998,24617705))
+def insertion_sort(arr):
+    for i in range(len(arr)):
+        local_min=min(arr[i:])
+        arr.remove(local_min)
+        arr.insert(i,local_min)    
+    return arr
+def recursive_insertion_sort(arr,acc=[]):
+    if arr==[]:
+        return acc
+    else:
+        acc.append(min(arr))
+        arr.remove(min(arr))
+        return(recursive_insertion_sort(arr,acc))
+
+l=[6,5,3,2,1]
+print(insertion_sort(l))
