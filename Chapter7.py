a896dd12a953e85e43e02fe1991a629ed83be766
A. Aboud <a.aboud10@gmail.com>
2019-10-31 12:13:00 -0400

    31-Oct-2019

---
+++ Chapter7.py
@@ -0,0 +1,19 @@
+#Trees practice
+
+class tree(self):
+    def __int__(data):
+        self.data=data
+        self.left=None
+        self.right=None
+
+    def addRight(data):
+        self.right=tree(data)
+
+    def addLeft(data):
+        self.left=tree(data)
+
+    def getLeft():
+        return self.left
+    
+    def getRight():
+        return self.right
