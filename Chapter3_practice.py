7c611c1f54461b60bac9eabc143fdfd4c824b2c5
A. Aboud <a.aboud10@gmail.com>
2019-10-31 12:29:25 -0400

    31-oct-2019

--- Chapter3_practice.py
+++ Chapter3_practice.py
@@ -1,5 +1,4 @@
 #practice building a linked list from scratch
-from pythonds.basic import Stack
 class Node():
 
     def __init__(self, initial_data):
@@ -18,68 +17,70 @@ class Node():
     def set_next(self, node):
         self.next=node
 
-class Unordered_list():
-    def __init__(self):
-        self.head = None
+# class Unordered_list():
+#     def __init__(self):
+#         self.head = None
 
-    def is_empty(self):
-        return self.head == None
+#     def is_empty(self):
+#         return self.head == None
 
-    def add(self, item):
-        temp = Node(item)
-        temp.set_next(self.head)
-        self.head = temp
+#     def add(self, item):
+#         temp = Node(item)
+#         temp.set_next(self.head)
+#         self.head = temp
 
-    def __str__(self):
-        temp=[]
-        current = self.head
-        while current!=None:
-            temp.append(current.get_data())
-            current=current.get_next()
-        return str(temp)
+#     def __str__(self):
+#         temp=[]
+#         current = self.head
+#         while current!=None:
+#             temp.append(current.get_data())
+#             current=current.get_next()
+#         return str(temp)
 
-    def size(self):
-        current = self.head
-        count= 0 
-        while current != None:
-            count+=1
-            current = current.get_next()
-        return count
+#     def size(self):
+#         current = self.head
+#         count= 0 
+#         while current != None:
+#             count+=1
+#             current = current.get_next()
+#         return count
 
-    def search(self,item):
-        current = self.head
-        found=False
-        while current!=None and found==False:
-            if current.get_data()==item:
-                found=True
-                break
-            else:
-                current=current.get_next()
-        return found
+#     def search(self,item):
+#         current = self.head
+#         found=False
+#         while current!=None and found==False:
+#             if current.get_data()==item:
+#                 found=True
+#                 break
+#             else:
+#                 current=current.get_next()
+#         return found
         
-    def remove(self,item):
-        current = self.head
-        found = False
-        previous = None
-        while not found:
-            if current.get_data()==item:
-                found = True
-            else:
-                previous = current
-                current = current.get_next()
-        if previous == None:
-            self.head = current.get_next()
-        else:
-            previous.set_next(current.get_next())
+#     def remove(self,item):
+#         current = self.head
+#         found = False
+#         previous = None
+#         while not found:
+#             if current.get_data()==item:
+#                 found = True
+#             else:
+#                 previous = current
+#                 current = current.get_next()
+#         if previous == None:
+#             self.head = current.get_next()
+#         else:
+#             previous.set_next(current.get_next())
 
-def convertBinary(num, stack=Stack()):
-    if num ==0:
-        res=""
-        while not (stack.isEmpty()):
-            res+=str(stack.pop())
-        return res
-    else:
-        stack.push(num%2)
-        convertBinary(num//2,stack)
+# def convertBinary(num, stack=Stack()):
+#     if num ==0:
+#         res=""
+#         while not (stack.isEmpty()):
+#             res+=str(stack.pop())
+#         return res
+#     else:
+#         stack.push(num%2)
+#         convertBinary(num//2,stack)
 
-print(convertBinary(1132))
+# print(convertBinary(1132))
+
+n=Node(4)
--- Chapter7.py
+++ Chapter7.py
@@ -1,19 +1,35 @@
 #Trees practice
-
-class tree(self):
-    def __int__(data):
-        self.data=data
+class Tree():
+    def __init__(self,data):
+        self.root=data
         self.left=None
         self.right=None
 
-    def addRight(data):
-        self.right=tree(data)
+    def addRight(self,data):
+        self.right=Tree(data)
 
-    def addLeft(data):
-        self.left=tree(data)
+    def addLeft(self,data):
+        self.left=Tree(data)
 
-    def getLeft():
+    def getLeft(self):
         return self.left
     
-    def getRight():
+    def getRight(self):
         return self.right
+
+    def __str__(self):
+        if self.getLeft()==None and self.getRight()==None:
+            print (str(root))
+        else:
+            res=[]
+            res.append((self.getLeft().__str__()))
+            res.append((self.getRight().__str__()))
+            print(str(res))
+
+t=Tree(4)
+t.addLeft('branch1')
+t.addRight('branch2')
+t.getLeft().addLeft('branch3')
+t.getRight().addRight('branch4')
+
+t.__str__()
