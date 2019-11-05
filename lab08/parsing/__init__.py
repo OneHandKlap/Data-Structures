b8aa44e60f488397231958820776ac1e87d1ee61
A. Aboud <a.aboud10@gmail.com>
2019-11-05 13:22:13 -0500

    5-nov-2019

--- lab08/parsing/__init__.py
+++ lab08/parsing/__init__.py
@@ -19,12 +19,12 @@ def parseHelper(expr, operators, operands):
             return operands[0]
         else:
             return handleOp([], operators, operands)
-    if isinstance(expr[0],list):
-        return parseHelper(expr[1:],operators,[parseHelper(expr[0],[],[])]+operands)
+    if isinstance(expr[0],list): #conditional to check for list item
+        return parseHelper(expr[1:],operators,[parseHelper(expr[0],[],[])]+operands) #essentially this just reruns the parsehelper on the list item as if it was its own unit, then returnsit as part of operands
     if numberp(expr[0]):
         return parseHelper(expr[1:], operators, [[expr[0], [], []]]+operands)
     elif operators == [] or precedence(expr[0]) > precedence(operators[0]):
-        if expr[0]=='!':
+        if expr[0]=='!': #check for the new factorial operator, and return a unique expression in list of lists notation
             return ([expr[0],operands.pop(),[]])
         else:
             return parseHelper(expr[1:], [expr[0]]+operators, operands)
